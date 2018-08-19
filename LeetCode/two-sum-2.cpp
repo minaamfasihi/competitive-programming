3730dd2ce14c12eafe20e48e7e212dc67ee061ea
MinFas <minaam.fasihi@gmail.com>
2018-08-20 01:06:05 +0500

    two-sum-2

---
+++ LeetCode/two-sum-2.cpp
@@ -0,0 +1,15 @@
+vector<int> twoSum(vector<int>& numbers, int target) {
+	int j = numbers.size() - 1; int i = 0;
+	vector<int> result;
+	while (i < j) {
+		if ((numbers[i] + numbers[j]) > target) j--;
+		else if ((numbers[i] + numbers[j]) < target) i++;
+		else {
+			result.push_back(i + 1);
+			result.push_back(j + 1);
+			break;
+		}
+	}
+	for (auto& i : result) cout << i << endl;
+	return result;
+}
