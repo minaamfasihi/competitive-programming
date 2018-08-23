799dc2019f6a0803b814f3df3b24af554c906406
MinFas <minaam.fasihi@gmail.com>
2018-08-23 22:18:42 +0500

    Update print-bits.cpp

--- others/print-bits.cpp
+++ others/print-bits.cpp
@@ -1,5 +1,7 @@
 void printBits(int num) {
-	unsigned int checkBit = 1 << (sizeof(int) * 8 - 1);
+	unsigned int checkBit = 1 << (sizeof(int) * 8 - 1); // unsigned int is important because simple int will have fill bits of 1 
+							    // since they preserve the sign of the integer. 
+							    // 1 at the MSB usually means a negative number. Hence, unsigned int
 	while (checkBit != 0) {
 		int result = checkBit & num;
 		if (result == checkBit) cout << "1";
