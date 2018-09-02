b9ee8dd266a05c3c55c410f7aabe087bddc04bc8
MinFas <minaam.fasihi@gmail.com>
2018-09-02 20:45:08 +0500

    shorter version of detect cycle

--- LeetCode/detect-cycle-in-linked-list.cpp
+++ LeetCode/detect-cycle-in-linked-list.cpp
@@ -21,3 +21,17 @@ bool hasCycle(ListNode *head) {
 	}
 	return false;
 }
+
+// Shorter version
+
+bool hasCycle(ListNode *head) {
+	if (!head) return false;
+
+	ListNode* fast = head; ListNode* slow = head;
+	while (fast && fast->next) {
+	    slow = slow->next;
+	    fast = fast->next->next;
+	    if (slow == fast) return true;
+	}
+	return false;
+}
