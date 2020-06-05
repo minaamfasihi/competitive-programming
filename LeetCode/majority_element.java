de8d283665085184a4a07989a53230c5e47abfb3
MinFas <minaam.fasihi@gmail.com>
2020-06-06 01:40:19 +0500

    169. Majority Element

---
+++ LeetCode/majority_element.java
@@ -0,0 +1,19 @@
+public int majorityElement(int[] nums) {
+    if (nums == null || nums.length == 0) return -1;
+
+    int cand = nums[0];
+    int count = 1;
+    for (int i = 1; i < nums.length; i++) {
+        if (cand == nums[i]) count++;
+        else if (count > 0) count--;
+        else {
+            count = 1;
+            cand = nums[i];
+        }
+    }
+    count = 0;
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] == cand) count++;
+    }
+    return count > (nums.length / 2) ? cand : -1;
+}
